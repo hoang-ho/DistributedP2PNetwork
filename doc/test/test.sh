b32d86c531484aa4f2ba268a3e3a13ffcb436247
hoang-ho <hazardhoang2610@gmail.com>
2021-03-13 16:23:53 -0600

    Partial EC2 deployment

--- doc/test/test.sh
+++ doc/test/test.sh
@@ -1,4 +1,31 @@
-cd ,,/..
-
 echo "This script is to run the test!"
+echo ""
+echo "Make sure that you have configure AWS access key!"
+echo ""
+echo "Create a key-pair 677kp if you haven't"
+echo ""
+echo "If you encounter any issue with running aws, try troubleshooting with this https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/TroubleshootingInstancesConnecting.html#TroubleshootingInstancesConnectingSSH"
+echo ""
+echo "Let's start!"
 
+# Compute the code
+cd ../..
+
+./gradlew clean build
+
+# Configure the network
+gradle p2pConfigure
+
+# Start an EC2 instance
+aws ec2 run-instances --image-id ami-0fc61db8544a617ed --instance-type t2.micro --key-name 677kp > instance.json
+
+# Get instance Id
+InstanceId=$(grep '^ *"InstanceId":' instance.json | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
+
+aws ec2 describe-instances --instance-id $InstanceId > runningInstance.json
+
+PublicDnsName=$(grep -m 1 '^ *"PublicDnsName":' runningInstance.json | awk '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
+
+scp -i "677kp.pem" Config.txt ec2-user@$PublicDnsName
+
+ssh -i "677kp.pem" ec2-user@$PublicDnsName
